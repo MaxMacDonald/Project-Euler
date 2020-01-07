73404eb09cc18d66389c8d2ca0157bd14cf010ff
Max MacDonald <Maximacd@gmail.com>
2020-01-07 19:40:59 +0000

    Added a timer and allow the user to select which project to run

---
+++ Project Euler/Admin/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_Euler.Admin
+{
+    class Class1
+    {
+    }
+}
--- Project Euler/Admin/MainClass.cs
+++ Project Euler/Admin/MainClass.cs
@@ -1,17 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
+using System.Reflection;
 using Project_Euler.Projects;
 
 namespace Project_Euler
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
-            Project4.Solution();
-            
+            string chosenProject;
+            string chosenNumber;
+
+            Console.WriteLine("Select which Project youd like to run:");
+            chosenNumber = Console.ReadLine();
+            chosenProject = "Project_Euler.Projects.Project" + chosenNumber;
+            Console.WriteLine(chosenProject);
+
+            Type t = Type.GetType(chosenProject);
+            MethodInfo method = t.GetMethod("Solution", BindingFlags.Static | BindingFlags.Public);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            method.Invoke(null, null);
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+            //Formating of Timer
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("Runtime " + elapsedTime);
+
+           
         }
     }
 }
